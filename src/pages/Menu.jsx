c3372f8726d09180c89c36e8ab6606c0726571c1
Luca-Mangiacotti <lucamangiacotti100@gmail.com>
2025-02-07 15:12:53 +0100

    feat: import axios and hooks

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,3 +1,10 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 export default function Menu() {
-  return <h3>menu delle pizze</h3>;
+  return (
+    <div>
+      <h3>menu delle pizze</h3>
+    </div>
+  );
 }
--- src/pages/pages-components/HeaderNav.jsx
+++ src/pages/pages-components/HeaderNav.jsx
@@ -1,10 +1,10 @@
 import { Link, NavLink } from "react-router-dom";
 export default function HeaderNav() {
   return (
-    <nav>
+    <div>
       <NavLink to="/">Home Page</NavLink>
       <NavLink to="/menu">Menu</NavLink>
       <NavLink to="/about-us">Info</NavLink>
-    </nav>
+    </div>
   );
 }
