de553b4f2ac77be0471cee0101d9e9280bc8b9af
Luca-Mangiacotti <lucamangiacotti100@gmail.com>
2025-02-07 15:52:01 +0100

    feat: add fetching of pizzas in menu page

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,10 +1,37 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
+const initialState = {
+  id: "",
+  name: "",
+  image: ``,
+  ingredients: [""],
+};
+
 export default function Menu() {
+  const [PizzasList, setPizzasList] = useState([]);
+
+  const fetchPosts = () => {
+    axios.get("http://127.0.0.1:3001/pizzas").then(function (res) {
+      setPizzasList(res.data);
+      console.log(res.data);
+    });
+  };
+
+  useEffect(fetchPosts, []);
+
   return (
     <div>
       <h3>menu delle pizze</h3>
+      <ul>
+        {PizzasList.map((pizza) => (
+          <div key={pizza.id}>
+            <h3>{pizza.name}</h3>
+            <img src={pizza.image} alt={pizza.name} />
+            <p>{pizza.ingredients}</p>
+          </div>
+        ))}
+      </ul>
     </div>
   );
 }
