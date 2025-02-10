0c312a2bac655d1cd55b04cf42f479320cd76713
Luca-Mangiacotti <lucamangiacotti100@gmail.com>
2025-02-10 20:24:02 +0100

    feat: add style on page not found

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,9 +1,16 @@
 import { Link } from "react-router-dom";
 export default function PageNotFound() {
   return (
-    <div className="container max-w-5xl mx-auto p-7 text-center">
+    <div className="container max-w-5xl mx-auto p-7 text-center items-center">
       <h1>ERRORE!</h1>
-      <h2>PAGINA NON TROVATA :/</h2>
+      <h2>
+        <s>PIZZA</s> PAGINA NON TROVATA
+      </h2>
+      <img
+        className="mx-auto"
+        src="../../images/pizzanotfound.png"
+        alt="not found"
+      />
       <div>
         <Link to={`/menu/`}>TORNA AL MENU</Link>
       </div>
